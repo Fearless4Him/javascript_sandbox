83a198d36acd2e45377a70750975fb2d8890a8da
Fearless4Him <107826307+Fearless4Him@users.noreply.github.com>
2024-07-21 20:34:56 -0400

    Dev: Added generateNavbar & toggleDarkMode to utils.js

---
+++ js/utils.js
@@ -0,0 +1,47 @@
+export function generateNavbar(items) {
+    const navUl = document.getElementById("nav-ul");
+    items.forEach((item) => {
+      const a = document.createElement("a");
+      const li = document.createElement("li");
+      a.href = item.link;
+      a.innerHTML = `<i class="fas ${item.icon}"></i>`;
+      a.appendChild(document.createTextNode(item.name));
+      a.appendChild(li);
+      navUl.appendChild(a);
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    generateNavbar(navigation_items);
+  });
+  
+  generateNavbar();
+
+  export function toggleDarkMode() {
+    const body = document.body;
+    const lightModeBtn = document.getElementById('lightMode');
+    const darkModeBtn = document.getElementById('darkMode');
+  
+    const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+  
+    if (isDarkMode) {
+      body.classList.add('darkMode');
+      darkModeBtn.classList.add('hidden');
+      lightModeBtn.classList.remove('hidden');
+    }
+  
+    lightModeBtn.addEventListener('click', () => {
+      body.classList.remove('darkMode');
+      darkModeBtn.classList.remove('hidden');
+      lightModeBtn.classList.add('hidden');
+      localStorage.setItem('darkMode', 'disabled');
+    });
+    darkModeBtn.addEventListener('click', () => {
+      body.classList.add('darkMode');
+      darkModeBtn.classList.add('hidden');
+      lightModeBtn.classList.remove('hidden');
+      localStorage.setItem('darkMode', 'enabled');
+    });
+  }
+  
+  toggleDarkMode();
